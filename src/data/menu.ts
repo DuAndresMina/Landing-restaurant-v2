f1235e190ec3ab14ac986f692afa42cea6e66601
Duvan Mina <129365067+DuAndresMina@users.noreply.github.com>
2024-11-13 04:16:45 -0500

    First commit

---
+++ src/data/menu.ts
@@ -0,0 +1,238 @@
+export const menu = [
+	{
+		name: "Hamburguesa Clásica",
+		price: 8.99,
+		description: "Carne de res, lechuga, tomate, cebolla y nuestra salsa especial",
+		image: "/food/hamburguesaclasica.webp"
+	},
+	{
+		name: "Hamburguesa con Queso",
+		price: 9.99,
+		description: "Nuestra clásica con queso cheddar derretido",
+		image: "/food/hamburguesaqueso.webp"
+	},
+	{
+		name: "Hamburguesa BBQ",
+		price: 10.99,
+		description: "Con bacon, cebolla caramelizada y salsa BBQ",
+		image: "/food/hbqq.webp"
+	},
+	{
+		name: "Hot Dog Supremo",
+		price: 7.99,
+		description: "Salchicha premium con toppings variados",
+		image: "/food/prdoble.webp"
+	},
+	{
+		name: "Alitas BBQ",
+		price: 11.99,
+		description: "8 piezas de alitas con salsa BBQ",
+		image: "/food/alas.webp"
+	},
+	{
+		name: "Alitas Picantes",
+		price: 11.99,
+		description: "8 piezas de alitas con salsa buffalo",
+		image: "/food/alashot.webp"
+	},
+	{
+		name: "Nuggets de Pollo",
+		price: 8.99,
+		description: "10 piezas con salsa a elección",
+		image: "/food/hug.webp"
+	},
+	{
+		name: "Papas Fritas",
+		price: 4.99,
+		description: "Crujientes y doradas",
+		image: "/food/pp.webp"
+	},
+	{
+		name: "Aros de Cebolla",
+		price: 5.99,
+		description: "Dorados y crujientes",
+		image: "/food/aros.webp"
+	},
+	{
+		name: "Ensalada César",
+		price: 8.99,
+		description: "Lechuga, pollo, crutones y aderezo césar",
+		image: "/food/ecesar.webp"
+	},
+	{
+		name: "Pizza Margherita",
+		price: 12.99,
+		description: "Salsa de tomate, mozzarella y albahaca",
+		image: "/food/pizza.webp"
+	},
+	{
+		name: "Pizza Pepperoni",
+		price: 13.99,
+		description: "Con abundante pepperoni y queso",
+		image: "/food/pizzape.webp"
+	},
+	{
+		name: "Sandwich de Pollo",
+		price: 9.99,
+		description: "Pollo a la parrilla con lechuga y mayonesa",
+		image: "/food/sanpollo.webp"
+	},
+	{
+		name: "Wrap Vegetal",
+		price: 8.99,
+		description: "Vegetales frescos con hummus",
+		image: "/food/wrap.webp"
+	},
+	{
+		name: "Nachos Supremos",
+		price: 10.99,
+		description: "Con queso, guacamole, jalapeños y crema agria",
+		image: "/food/nachos.webp"
+	},
+	{
+		name: "Salchipapa",
+		price: 10.99,
+		description: "Salchicha, papa, queso, salsas",
+		image: "/food/salchi.webp"
+	},
+	{
+		name: "Papa Rellena",
+		price: 10.99,
+		description: "Papita rellena, arroz, huevo, carne, etc",
+		image: "/food/pp.webp"
+	},
+	{
+		name: "Pescado",
+		price: 10.99,
+		description: "Pecao wacuco",
+		image: "/food/pescao.webp"
+	}
+	
+	
+];
+
+export const drinks = [
+	{
+		name: "Coca-Cola",
+		price: 2.99,
+		description: "Clásica bebida refrescante",
+		image: "/menu/coke.webp"
+	},
+	{
+		name: "Sprite",
+		price: 2.99,
+		description: "Refresco de lima-limón",
+		image: "/menu/sprite.webp"
+	},
+	{
+		name: "Fanta",
+		price: 2.99,
+		description: "Refresco de naranja",
+		image: "/menu/fanta.webp"
+	},
+	{
+		name: "Limonada Natural",
+		price: 3.99,
+		description: "Recién exprimida",
+		image: "/menu/lemonade.webp"
+	},
+	{
+		name: "Té Helado",
+		price: 3.49,
+		description: "Con limón y menta",
+		image: "/menu/ice-tea.webp"
+	},
+	{
+		name: "Agua Mineral",
+		price: 1.99,
+		description: "Con o sin gas",
+		image: "/menu/water.webp"
+	},
+	{
+		name: "Cerveza",
+		price: 4.99,
+		description: "Variedad de marcas",
+		image: "/menu/beer.webp"
+	},
+	{
+		name: "Malteada de Chocolate",
+		price: 5.99,
+		description: "Cremosa y deliciosa",
+		image: "/menu/milkshake-chocolate.webp"
+	},
+	{
+		name: "Malteada de Vainilla",
+		price: 5.99,
+		description: "Suave y cremosa",
+		image: "/menu/milkshake-vanilla.webp"
+	},
+	{
+		name: "Café Americano",
+		price: 2.99,
+		description: "Café recién hecho",
+		image: "/menu/coffee.webp"
+	}
+];
+
+export const desserts = [
+	{
+		name: "Brownie con Helado",
+		price: 6.99,
+		description: "Caliente con helado de vainilla",
+		image: "/menu/brownie.webp"
+	},
+	{
+		name: "Cheesecake",
+		price: 5.99,
+		description: "Con salsa de frutos rojos",
+		image: "/menu/cheesecake.webp"
+	},
+	{
+		name: "Helado Sundae",
+		price: 4.99,
+		description: "3 bolas con toppings",
+		image: "/menu/sundae.webp"
+	},
+	{
+		name: "Tarta de Manzana",
+		price: 5.99,
+		description: "Caliente con canela",
+		image: "/menu/apple-pie.webp"
+	},
+	{
+		name: "Cookies",
+		price: 3.99,
+		description: "3 galletas con chispas de chocolate",
+		image: "/menu/cookies.webp"
+	},
+	{
+		name: "Flan",
+		price: 4.99,
+		description: "Casero con caramelo",
+		image: "/menu/flan.webp"
+	},
+	{
+		name: "Tiramisú",
+		price: 6.99,
+		description: "Clásico postre italiano",
+		image: "/menu/tiramisu.webp"
+	},
+	{
+		name: "Mousse de Chocolate",
+		price: 5.99,
+		description: "Suave y cremoso",
+		image: "/menu/mousse.webp"
+	},
+	{
+		name: "Banana Split",
+		price: 7.99,
+		description: "Con 3 sabores de helado",
+		image: "/menu/banana-split.webp"
+	},
+	{
+		name: "Crepe de Nutella",
+		price: 6.99,
+		description: "Con fresas y crema batida",
+		image: "/menu/crepe.webp"
+	}
+];
