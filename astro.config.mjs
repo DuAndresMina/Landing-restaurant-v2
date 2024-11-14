6b09ae34943622951dbd9ccf7cda29a73cb41cb6
Duvan Mina <129365067+DuAndresMina@users.noreply.github.com>
2024-11-14 00:55:33 -0500

    astro config Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://duandresmina.github.io',
-    base: 'Restaurante-lading-V2',
+    base: 'Landing-restaurant-v2',
   });
