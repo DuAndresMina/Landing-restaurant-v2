c136d68367807448cb987c1342229068904be39b
Duvan Mina <129365067+DuAndresMina@users.noreply.github.com>
2024-11-14 01:48:14 -0500

    Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://duandresmina.github.io',
-    base: 'Landing-restaurant-v2',
-  });
+  //site: 'https://duandresmina.github.io',
+  //base: 'Landing-restaurant-v2',
+  output: 'server',
+  adapter: vercel()
+});
